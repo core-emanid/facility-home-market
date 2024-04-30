5d3399b0645e322686e8c5beddc99bc412193d7c
Rafael <rafael_dvid@hotmail.com>
2024-04-30 20:40:33 -0300

    fix import paths for bg image and tab icon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
 	theme: {
 		extend: {
 			backgroundImage: {
-				'home': "url('./src/images/Banner-inicial.png')",
+				'home': "url('../src/images/Banner-inicial.png')",
 			}
 		},	
 	},
